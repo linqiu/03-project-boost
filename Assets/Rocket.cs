7dc09a3bec304f175b30be05afee31e13012be94
Lin Qiu <theonlylinqiu@gmail.com>
2019-07-04 00:22:39 -0400

    Add sounds for death and success

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,8 +5,14 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rcsThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
     Rigidbody rigidBody;
     AudioSource rocketAudio;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +23,52 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrust();
+            Rotate();
+        }       
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void ProcessSuccess()
+    {
+        state = State.Transcending;
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(success);
+        Invoke("LoadNextScene", 1f);
+    }
+
+    void ProcessDeath()
+    {
+        state = State.Dying;
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(death);
+        Invoke("LoadFirstScene", 2f);
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; }   
+
         switch(collision.gameObject.tag)
         {
             case "Friendly":
                 break;
             case "Finish":
-                print("hit finish");
-                SceneManager.LoadScene(1);
+                ProcessSuccess();
                 break;
             default:
-                SceneManager.LoadScene(0);
+                ProcessDeath();
                 break;
         }
 
@@ -54,19 +90,24 @@ public class Rocket : MonoBehaviour
         rigidBody.freezeRotation = false;
     }
 
-    void Thrust()
+    void RespondToThrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!rocketAudio.isPlaying)
-            {
-                rocketAudio.Play();
-            }
+            ApplyThrust();
         }
         else
         {
             rocketAudio.Stop();
+        }
+    }
+
+    void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!rocketAudio.isPlaying)
+        {
+            rocketAudio.PlayOneShot(mainEngine);
         }
     }
 }
